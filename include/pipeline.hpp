71f27ee079c57803f9b3f0b08308b547ec8926bf
Thenaesh Elango <thenaeshelango@gmail.com>
2022-07-29 20:14:32 +0800

    Add pipeline creation code

---
+++ include/pipeline.hpp
@@ -0,0 +1,37 @@
+#ifndef VULKANSTATE_H
+#define VULKANSTATE_H
+#include <vulkanstate.hpp>
+#endif
+
+#ifndef SHADER_H
+#define SHADER_H
+#include <shader.hpp>
+#endif
+
+
+class Pipeline {
+
+    VulkanState const& state;
+
+    std::vector<Shader> shaders;
+
+    VkRenderPass renderPass;
+    VkPipelineLayout pipelineLayout;
+    VkPipeline pipeline;
+
+public:
+
+    Pipeline(VulkanState const& state, std::vector<Shader> shaders);
+    virtual ~Pipeline();
+
+private:
+
+    void createRenderPass();
+    void destroyRenderPass();
+
+    void createPipelineLayout();
+    void destroyPipelineLayout();
+
+    void createPipeline();
+    void destroyPipeline();
+};

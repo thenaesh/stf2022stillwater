dae204039ee9ce48f9584840f9ed59c3397f7cc2
Thenaesh Elango <thenaeshelango@gmail.com>
2022-07-29 20:17:25 +0800

    Construct shaders and pipeline

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,32 @@
 #include <vulkanstate.hpp>
 #endif
 
+#ifndef PIPELINE_H
+#define PIPELINE_H
+#include <pipeline.hpp>
+#endif
+
+using namespace std;
+
 
 int main(int argc, char** argv) {
     WindowState window{"Still Water", 1920, 1080};
     VulkanState vkstate{window};
+
+    vector<Shader> shaders;
+
+    shaders.push_back(Shader{
+        vkstate,
+        VK_SHADER_STAGE_VERTEX_BIT,
+        "/home/thenaesh/Documents/still_water_vulkan/shaders/vert.spv"
+    });
+    shaders.push_back(Shader{
+        vkstate,
+        VK_SHADER_STAGE_FRAGMENT_BIT,
+        "/home/thenaesh/Documents/still_water_vulkan/shaders/frag.spv"
+    });
+
+    Pipeline pipeline{vkstate, move(shaders)};
 
     while (window.isActive()) {
         glfwPollEvents();

6a128fdef6919f51bf634ff2224a0551fb8a4b2f
Thenaesh Elango <thenaeshelango@gmail.com>
2022-08-28 21:15:17 +0800

    Stop culling triangles drawn the wrong way

--- src/pipeline.cpp
+++ src/pipeline.cpp
@@ -172,7 +172,7 @@ void Pipeline::createPipeline() {
         .sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO,
         .depthClampEnable = VK_FALSE,
         .polygonMode = VK_POLYGON_MODE_FILL,
-        .cullMode = VK_CULL_MODE_BACK_BIT,
+        .cullMode = VK_CULL_MODE_NONE,
         .frontFace = VK_FRONT_FACE_CLOCKWISE,
         .depthBiasEnable = VK_FALSE,
         .depthBiasConstantFactor = 0.0f, // Optional
